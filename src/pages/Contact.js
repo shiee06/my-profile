89bdc20998629953cc8ff67584236edf532bd5dc
shiee06 <kiptejohnshielwyn18@gmail.com>
2025-04-30 11:54:18 +0800

    updated email

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ function Contact() {
     return (
       <div>
         <h1>Contact Me</h1>
-        <p>Email: your.email@example.com</p>
+        <p>Email: kiptejohnshielwyn18@gmail.com</p>
         <p>GitHub: <a href="https://github.com/shiee06" target="_blank" rel="noreferrer">shiee06</a></p>
         <p>LinkedIn: <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noreferrer">shiee06</a></p>
       </div>
